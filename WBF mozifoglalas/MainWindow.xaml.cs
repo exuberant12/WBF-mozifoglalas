5bd71f7c9a8d17afa0dcdd1a679f0e6758519836
takacsk2 <takacsk@310-05>
2025-12-15 09:58:48 +0100

    hozzáadás

--- WBF mozifoglalas/MainWindow.xaml.cs
+++ WBF mozifoglalas/MainWindow.xaml.cs
@@ -1,52 +1,65 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
 
 namespace WBF_mozifoglalas
 {
-    public class mozi
-    {
-        public string cim { get; set; }
-        public DateTime idopont { get; set; }
-        public string terem { get; set; }
-        public int szabadhelyek { get; set; }
-        public bool _3D { get; set; }
-
-        public mozi(string cím, DateTime idopont, string terem, int szabadhelyek, bool _3D)
-        {
-            this.cim = cím;
-            this.idopont = idopont;
-            this.terem = terem;
-            this.szabadhelyek = szabadhelyek;
-            this._3D = _3D;
-        }
-    }
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public ObservableCollection<mozi> mozifilmek = new ObservableCollection<mozi>();
+        // 🔹 Adatmodell
+        public class Mozi : INotifyPropertyChanged
+        {
+            private int _szabadhelyek;
+
+            public string cim { get; set; }
+            public DateTime idopont { get; set; }
+            public string terem { get; set; }
+
+            public int szabadhelyek
+            {
+                get => _szabadhelyek;
+                set
+                {
+                    _szabadhelyek = value;
+                    PropertyChanged?.Invoke(this,
+                        new PropertyChangedEventArgs(nameof(szabadhelyek)));
+                }
+            }
+
+            public bool _3D { get; set; }
+
+            public Mozi(string cim, DateTime idopont, string terem, int szabadhelyek, bool _3D)
+            {
+                this.cim = cim;
+                this.idopont = idopont;
+                this.terem = terem;
+                this.szabadhelyek = szabadhelyek;
+                this._3D = _3D;
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+        }
+
+        // 🔹 Lista
+        ObservableCollection<Mozi> mozifilmek;
+
         public MainWindow()
         {
             InitializeComponent();
-            mozifilmek.Add(new mozi("fnaf1", new DateTime(2025, 12, 15), "1-es terem", 12, true));
-            mozifilmek.Add(new mozi("fnaf2", new DateTime(2026, 12, 15), "2-es terem", 12, true));
-            mozifilmek.Add(new mozi("fnaf3", new DateTime(2027, 12, 15), "3-es terem", 12, true));
-            mozifilmek.Add(new mozi("fnaf4", new DateTime(2028, 12, 15), "4-es terem", 12, true));
-            mozifilmek.Add(new mozi("fnaf5", new DateTime(2029, 12, 15), "5-es terem", 12, true));
-            mozifilmek.Add(new mozi("fnaf6", new DateTime(2030, 12, 15), "6-es terem", 12, true));
-            
+
+            mozifilmek = new ObservableCollection<Mozi>()
+            {
+                new Mozi("FNAF 1", new DateTime(2025,12,15,18,0,0), "1-es terem", 12, true),
+                new Mozi("FNAF 2", new DateTime(2026,12,15,18,0,0), "2-es terem", 5, false),
+                new Mozi("FNAF 3", new DateTime(2027,12,15,18,0,0), "3-as terem", 0, true),
+                new Mozi("FNAF 4", new DateTime(2028,12,15,18,0,0), "4-es terem", 20, false),
+            };
+
+            DataGrid.ItemsSource = mozifilmek;
         }
-        
+
         private void adatokbetoltese(object sender, RoutedEventArgs e)
         {
             DataGrid.ItemsSource = mozifilmek;
@@ -54,26 +67,59 @@ namespace WBF_mozifoglalas
 
         private void foglalas(object sender, RoutedEventArgs e)
         {
+            if (DataGrid.SelectedItem is Mozi m)
+            {
+                if (m.szabadhelyek > 0)
+                    m.szabadhelyek--;
+                else
+                    MessageBox.Show("Nincs több szabad hely!");
+            }
+        }
 
-            if (DataGrid.SelectedItem is mozi)
-            {
-                ((mozi)DataGrid.SelectedItem).szabadhelyek 
-                    = ((mozi)DataGrid.SelectedItem).szabadhelyek - 1;
-                DataGrid.Items.Refresh();
-            }
-        }
-        private void vanehely(object sender, RoutedEventArgs e)
+        private void csakVanHely(object sender, RoutedEventArgs e)
         {
-            List<mozi> vanhely = new List<mozi>();
-            foreach ( var mozi in mozifilmek )
-            {
-                if ( mozi.szabadhelyek > 0 )
-                {
-                    vanhely.Add(mozi);
-                }
-            }
-            DataGrid.ItemsSource = vanhely;
-            DataGrid.Items.Refresh();
+            DataGrid.ItemsSource =
+                new ObservableCollection<Mozi>(mozifilmek.Where(x => x.szabadhelyek > 0));
         }
+
+        private void csak3D(object sender, RoutedEventArgs e)
+        {
+            DataGrid.ItemsSource =
+                new ObservableCollection<Mozi>(mozifilmek.Where(x => x._3D));
+        }
+
+        private void legnepszerubb(object sender, RoutedEventArgs e)
+        {
+            var film = mozifilmek.OrderBy(x => x.szabadhelyek).First();
+            MessageBox.Show($"Legnépszerűbb film:\n{film.cim}");
+        }
+
+        private void atlagSzabadHely(object sender, RoutedEventArgs e)
+        {
+            double atlag = mozifilmek.Average(x => x.szabadhelyek);
+            MessageBox.Show($"Átlagos szabad hely: {atlag:F1}");
+        }
+
+        private void hozzaadas(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                mozifilmek.Add(new Mozi(
+                    txtCim.Text,
+                    DateTime.Parse(txtIdopont.Text),
+                    txtTerem.Text,
+                    int.Parse(txtSzabad.Text),
+                    chk3D.IsChecked == true
+                ));
+
+                
+                DataGrid.ItemsSource = mozifilmek;
+            }
+            catch
+            {
+                MessageBox.Show("Hibás adat!");
+            }
+        }
+
     }
 }
